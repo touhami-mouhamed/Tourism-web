a67ee43b573bc03a71985344b898a29d97271dca
touhami-mouhamed <inquiredz@gmail.com>
2026-04-17 22:42:15 +0100

    adding responsiveness

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 let head = document.querySelector("header");
-window.addEventListener("scroll", () => {
-    head.classList.toggle("stick", window.scrollY > 150);
-});
+
+
+let menu = document.querySelector('.menu');
+let cancel = document.querySelector('.cancel');
+let slide = document.querySelector('.main-nav');
+let scrollBar = document.querySelector('body');
+
+menu.onclick = () => {
+    scrollBar.classList.add('no-overflow');
+    menu.classList.add('menu-disappear');
+    cancel.classList.add('cancel-appear');
+    slide.classList.add('slide-show');
+    head.classList.remove("stick");
+};
+
+cancel.onclick = () => {
+    scrollBar.classList.remove('no-overflow');
+    menu.classList.remove('menu-disappear');
+    cancel.classList.remove('cancel-appear');
+    slide.classList.remove('slide-show');
+    head.classList.add("stick");
+};
